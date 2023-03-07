0f59f729b640b3d86b20046bd862ef26c1043cc8
Vansh Uppal <vanshuppal2002@gmail.com>
2023-03-07 07:51:07 +0530

    Minor bug fixes
    git checkout main otherwise master and main becoming different

--- src/script.sh
+++ src/script.sh
@@ -3,8 +3,9 @@ tar -xf "katana_${2}_${1}.tar.gz" -C challenge/${2}
 cd "/opt/katana/challenge/${2}/${1}"
 git init
 git config --global --add safe.directory "/opt/katana/challenge/${2}/${1}"
-git remote add origin https://Personalaccesstokenhere@github.com/Perseus-Jackson477/notekeeper.git
+git remote add origin https://Personalaccesstoken@github.com/Perseus-Jackson477/notekeeper.git
 git branch -M main
 git add .
 git stash
 git pull origin main --allow-unrelated-histories
+git checkout main
--- src/setup_script.sh
+++ src/setup_script.sh
@@ -10,5 +10,5 @@ apt-get update -y
 apt install git -y
 mv /opt/katana/patch_challenge.sh /usr/bin/patch_challenge
 chmod +x /usr/bin/patch_challenge
-git config --global user.email "123@idkwhatdayitisanymore.com"
-git config --global user.name "KMN"
+git config --global user.email "vanshuppal2002@gmail.com"
+git config --global user.name "Perseus-Jackson477"
