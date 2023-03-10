a9abff02e813c1044aa023c89b19fb31f4248819
Vansh Uppal <vanshuppal2002@gmail.com>
2023-03-10 08:56:21 +0530

    Add sdslabs collaborator

--- src/script.sh
+++ src/script.sh
@@ -6,6 +6,7 @@ git init
 git config --global --add safe.directory "/opt/katana/challenge/${2}/${1}"
 curl -H "Content-Type: application/json" -X POST -d '{"name":"'${1}'","description":"'${1}'","private":true}' "http://$GOGS/api/v1/user/repos?token=$PASSWORD"
 curl -H "Content-Type: application/json" -X POST -d '{ "type": "gogs", "config": { "url": "https://random.free.beeceptor.com","content_type": "json"},"events": ["push"],"active": true}' "http://$GOGS/api/v1/repos/$USERNAME/${1}/hooks?token=$PASSWORD"
+curl -X PUT -H "Authorization: token $PASSWORD" -H "Content-Type: application/json" -d '{"permission": "admin"}' http://$GOGS/api/v1/repos/$USERNAME/${1}/collaborators/sdslabs
 git remote add origin "http://$PASSWORD@$GOGS/$USERNAME/${1}.git"
 git branch -M master
 git checkout master
