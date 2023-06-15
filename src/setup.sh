64c4e3c4b9f9fb4ae34bcc00222c9ee0b4ba8b26
Perseus-Jackson477 <vanshuppal2002@gmail.com>
2023-06-15 13:41:53 +0530

    fix bugs

--- src/setup.sh
+++ src/setup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 source /etc/profile
-tar -xf "/opt/katana/katana_${2}_${1}.tar.gz" -C /opt/katana/challenge/${2}
-rm -rf "/opt/katna/katana_${2}_${1}.tar.gz"
 if [ ! -d "$/opt/katana/challenge/${2}" ]; then
     mkdir "/opt/katana/challenge/${2}"
 fi
+tar -xf "/opt/katana/katana_${2}_${1}.tar.gz" -C /opt/katana/challenge/${2}
+rm -rf "/opt/katna/katana_${2}_${1}.tar.gz"
 cd "/opt/katana/challenge/${2}/${1}"
+if [ ! -d "/opt/katana/challenge/${2}/${1}/.git" ]; then
 git init
 git config --global --add safe.directory "/opt/katana/challenge/${2}/${1}"
 curl -H "Content-Type: application/json" -X POST -d '{"name":"'${1}'","description":"'${1}'","private":true}' "http://$GOGS/api/v1/user/repos?token=$PASSWORD"
-curl -H "Content-Type: application/json" -X POST -d '{ "type": "gogs", "config": { "url": "'"$BACKEND_URL"'","content_type": "json"},"events": ["push"],"active": true}' "http://$GOGS/api/v1/repos/$USERNAME/${1}/hooks?token=$PASSWORD"
-curl -X PUT -H "Authorization: token $PASSWORD" -H "Content-Type: application/json" -d '{"permission": "admin"}' http://$GOGS/api/v1/repos/$USERNAME/${1}/collaborators/$ADMIN
 git remote add origin "http://$PASSWORD@$GOGS/$USERNAME/${1}.git"
 git branch -M master
 git checkout master
 git add .
 git commit -m "${1} challenge of $USERNAME" 
 git push -u origin master -f
+
+curl -H "Content-Type: application/json" -X POST -d '{ "type": "gogs", "config": { "url": "'"$BACKEND_URL"'","content_type": "json"},"events": ["push"],"active": true}' "http://$GOGS/api/v1/repos/$USERNAME/${1}/hooks?token=$PASSWORD"
+curl -X PUT -H "Authorization: token $PASSWORD" -H "Content-Type: application/json" -d '{"permission": "admin"}' http://$GOGS/api/v1/repos/$USERNAME/${1}/collaborators/$ADMIN
+fi
