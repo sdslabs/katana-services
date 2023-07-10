d163bd751b8ef698d06d2db2a315d39ff694cbf4
Aditya Bisht <adityabisht64@gmail.com>
2023-07-11 02:59:56 +0530

    Add watch to Kashira's annotation

--- Kashira/src/app.py
+++ Kashira/src/app.py
@@ -1,8 +1,11 @@
 import random
-from kubernetes import config, client
+from kubernetes import config, client, watch
 from kubernetes.stream import stream
 
 flag_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
+
+config.load_incluster_config()
+api = client.CoreV1Api()
 
 def generate_flag():
     # Random length of flag between 10 and 20
@@ -12,8 +15,6 @@ def generate_flag():
     return flag
 
 def pod_executor(command, pod_name, pod_namespace):
-    config.load_incluster_config()
-    api = client.CoreV1Api()
     resp = api.read_namespaced_pod(name=pod_name, namespace=pod_namespace)
     exec_command = [
         '/bin/sh',
@@ -26,3 +27,18 @@ def pod_executor(command, pod_name, pod_namespace):
                   stderr=True, stdin=False,
                   stdout=True, tty=False)
     return resp
+
+def update_flag():
+    pass
+    
+if __name__ == '__main__':
+    w = watch.Watch()
+    namespace = 'katana'
+    statefulset_name = 'kashira'
+    for event in w.stream(api.list_namespaced_stateful_set, namespace=namespace):
+        if event['object'].metadata.name == statefulset_name and 'tick' in event['object'].metadata.annotations:
+            update_flag()
+
+            statefulset = api.read_namespaced_stateful_set(name=statefulset_name, namespace=namespace)
+            statefulset.metadata.annotations['tick'] = 'false'
+            api.patch_namespaced_stateful_set(name=statefulset_name, namespace=namespace, body=statefulset)
