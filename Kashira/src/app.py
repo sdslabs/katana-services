3a4ff0e474fdc42370220552d79cb6eaddd8a4e5
Perseus-Jackson477 <vanshuppal2002@gmail.com>
2023-07-12 07:19:02 +0530

    add flag updater

--- Kashira/src/app.py
+++ Kashira/src/app.py
@@ -1,49 +1,72 @@
-import random
+from flask import Flask
 from kubernetes import config, client, watch
 from kubernetes.stream import stream
-from flask import Flask
+from pymongo import MongoClient
+import random
+import threading
 
 app = Flask(__name__)
-
 flag_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
 
 config.load_incluster_config()
 api = client.CoreV1Api()
+api_instance = client.AppsV1Api()
+
+def establish_mongo_connection(username, password):
+    service_name = "mongo-nodeport-svc"
+    namespace = "katana"
+    service = api.read_namespaced_service(name=service_name, namespace=namespace)
+    mongo_ip = service.spec.cluster_ip
+    mongo_uri = f"mongodb://{username}:{password}@{mongo_ip}"
+    mongo_client = MongoClient(mongo_uri)
+    print("Connected to MongoDB.")
+    return mongo_client
+
+mongo = establish_mongo_connection("adminuser","password123")
 
 def generate_flag():
     length_of_flag = random.randint(15, 20)
-
     flag = 'katana{' + ''.join(random.choice(flag_chars) for _ in range(length_of_flag)) + '}'
     return flag
 
-def pod_executor(command, pod_name, pod_namespace):
-    resp = api.read_namespaced_pod(name=pod_name, namespace=pod_namespace)
-    exec_command = [
-        '/bin/sh',
-        '-c',
-        ' '.join(command)]
-    resp = stream(api.connect_get_namespaced_pod_exec,
-                  pod_name,
-                  pod_namespace,
-                  command=exec_command,
-                  stderr=True, stdin=False,
-                  stdout=True, tty=False)
-    return resp
+def update_flag(mongo_client, team_name, challenge_name):
+    flag = generate_flag()  
+    mongo_db = mongo_client['katana']
+    mongo_collection = mongo_db['teams']
+    
+    query = {
+        'username': team_name,
+        'challenges.challengename': challenge_name
+    }
+    
+    update = {
+        '$set': {
+            'challenges.$.flag': flag
+        }
+    }
+    mongo_collection.update_one(query, update)
 
-def update_flag():
-    pass
-
-@app.route('/')
-def hello():
-    return "Hello, world!"
-
-if __name__ == '__main__':
-    w = watch.Watch()
+def watch_statefulset():
     namespace = 'katana'
     statefulset_name = 'kashira'
-    for event in w.stream(api.list_namespaced_stateful_set, namespace=namespace):
-        if event['object'].metadata.name == statefulset_name and 'tick' in event['object'].metadata.annotations:
-            update_flag()
-            statefulset = api.read_namespaced_stateful_set(name=statefulset_name, namespace=namespace)
-            statefulset.metadata.annotations['tick'] = 'false'
-            api.patch_namespaced_stateful_set(name=statefulset_name, namespace=namespace, body=statefulset)
+
+    w = watch.Watch()
+    for event in w.stream(api_instance.list_namespaced_stateful_set, namespace=namespace):
+        if event['object'].metadata.name == statefulset_name:
+            annotations = event['object'].metadata.annotations
+            if annotations and annotations.get('tick') == 'true':
+                update_flag(mongo,"katana-team-0","notekeeper")
+                annotations['tick'] = 'false'  # Set annotation to 'false'
+                statefulset = api_instance.read_namespaced_stateful_set(statefulset_name, namespace)
+                statefulset.metadata.annotations = annotations
+                updated_statefulset = api_instance.patch_namespaced_stateful_set(statefulset_name, namespace, statefulset)
+
+def run_watch_statefulset():
+    with app.app_context():
+        watch_statefulset()
+
+
+if __name__ == '__main__':
+    t = threading.Thread(target=run_watch_statefulset)
+    t.start()
+    app.run()
